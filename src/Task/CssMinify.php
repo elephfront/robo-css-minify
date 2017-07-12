e272747ea23c6f717656f44587ead434a033a678
Yves P <havokinspiration@gmail.com>
2017-07-12 21:45:01 +0200

    Add a copyright notice to all key files

--- src/Task/CssMinify.php
+++ src/Task/CssMinify.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Copyright (c) Yves Piquel (http://www.havokinspiration.fr)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Yves Piquel (http://www.havokinspiration.fr)
+ * @link          http://github.com/elephfront/robo-css-minify
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 declare(strict_types=1);
 namespace Elephfront\RoboCssMinify\Task;
 
--- tests/TestCase/Robo/Task/CssMinifyTest.php
+++ tests/TestCase/Robo/Task/CssMinifyTest.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Copyright (c) Yves Piquel (http://www.havokinspiration.fr)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Yves Piquel (http://www.havokinspiration.fr)
+ * @link          http://github.com/elephfront/robo-css-minify
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 namespace Elephfront\RoboCssMinify\Tests;
 
 use Elephfront\RoboCssMinify\Task\CssMinify;
